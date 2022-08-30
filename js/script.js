cc0f2ecb7c11392003e77f918ad801c5c65d9ae7
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-30 15:40:29 +0200

    create ciclo while in console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 console.log("Martedì");
+
+const listItems = [
+  "yogurt",
+  "pane",
+  "sale",
+  "marmellata",
+  "farina",
+  "affettati",
+  "lievito",
+  "mozzarelle",
+  "burro",
+  "acqua",
+  "olive",
+  "aceto",
+  "olio",
+  "broccoli",
+  "bagnoschiuma",
+  "birre",
+];
+/*
+console.log(listItems);
+
+for (let i = 0; i < listItems.length; i++) {
+  console.log(listItems[i]);
+}
+*/
+
+const elenco = document.querySelector(".list-group");
+
+let i = 0;
+
+while (i < listItems.length) {
+  console.log(listItems[i]);
+  i++;
+}
